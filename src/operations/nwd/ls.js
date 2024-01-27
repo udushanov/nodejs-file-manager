b7f17233f7b2a1aeb11030ccd58d9c7c72f1963c
Ulugbek <u.dushanov@gmail.com>
2024-01-27 15:13:57 +0500

    refactor: remove comments

--- src/operations/nwd/ls.js
+++ src/operations/nwd/ls.js
@@ -38,29 +38,4 @@ export const listDirectoryContents = async (currentDirectory) => {
   } catch (err) {
     console.error("Operation failed:", err.message);
   }
-
-  // try {
-  //   const contents = await fs.readdir(currentDirectory);
-  //   const directories = [];
-  //   const files = [];
-
-  //   for (const item of contents) {
-  //     const itemPath = path.join(currentDirectory, item);
-  //     const stats = await fs.stat(itemPath);
-
-  //     if (stats.isDirectory()) {
-  //       directories.push(item);
-  //     } else if (stats.isFile()) {
-  //       files.push(item);
-  //     }
-  //   }
-
-  //   const sortedContents = [...directories, ...files].sort();
-  //   console.log("\nList of contents:");
-  //   sortedContents.forEach((item) => {
-  //     console.log(item);
-  //   });
-  // } catch (error) {
-  //   console.error("Operation failed:", error.message);
-  // }
 };
